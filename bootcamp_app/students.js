a142f2c2aa092f5f72328de131c7eccee0dc2171
Polina Skrobot <chi.polly.no@gmail.com>
2021-09-16 02:15:50 +0000

    Parameterization

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,14 +7,15 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 //_______Easy query______
-
-pool.query(`
-SELECT students.id, students.name, cohorts.name as cohort_id
+const queryString = `SELECT students.id, students.name, cohorts.name as cohort_id
 FROM students
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;`;
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -13,19 +13,19 @@ pool.connect()
   })
   .catch(err => console.log('db connection error:', err));
 //_______Easy query______
-
-pool.query(`
-SELECT DISTINCT teachers.name as name, cohorts.name as cohort
+const queryString = `SELECT DISTINCT teachers.name as name, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
 JOIN students ON assistance_requests.student_id = students.id
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2]}'
-ORDER By teachers.name;
-`)
+WHERE cohorts.name = $1
+ORDER By teachers.name;`;
+
+const cohortName = process.argv[2];
+const value = [cohortName];
+pool.query(queryString, value)
   .then(res => {
     res.rows.forEach(teacher => {
       console.log(`${process.argv[2]}: ${teacher.name}`);
-      
     });
   });
